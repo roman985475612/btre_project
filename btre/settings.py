2e2d99d461ec59f851aeb4cc011e82084239a122
Roman <roman985475612@gmail.com>
2021-03-07 12:41:22 +0300

    Added local settings

--- btre/settings.py
+++ btre/settings.py
@@ -82,18 +82,6 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'btre',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
